402b3207cc6ecd4b9c72f6ad2ce5cc7d911843fc
Alan Brevick <mrmincho@gmail.com>
2018-07-29 17:09:48 -0400

    php

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -4,9 +4,9 @@ class observium::apache {
 
   include apache
   include apache::params
-
+  include apache::mod::php
   ensure_resource( 'apache::mod',  'rewrite' )
-  ensure_resource( 'apache::mod', ' php' )
+  # ensure_resource( 'apache::mod', ' php' )
 
   apache::vhost { 'observium':
     priority           => '10',
