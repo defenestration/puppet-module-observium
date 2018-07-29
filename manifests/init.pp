e8eaaf606f3d2dee0b8b4c3129ac5c776ce18c09
Alan Brevick <mrmincho@gmail.com>
2018-07-29 13:35:54 -0400

    fix shuffle

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,129 +1,129 @@
-    }
-    owner   => $rrd_owner,
-      $default_packages = 'observium'
-  $mysql_user                = undef,
-  $mysql_db                  = undef,
-    validate_array($devices)
-    owner   => $config_owner,
-  cron { 'discovery-new':
-#
-    command => "${base_path}/discovery.php -h all >> /dev/null 2>&1",
-  $rrd_owner                 = 'root',
-        name    => $my_packages,
-    }
-    path    => $config_path,
-    user    => $cron_discovery_new_user,
-    command     => 'php includes/update/update.php',
-  $cron_poller_minute        = '*/5',
-  $cron_discovery_new_user   = 'root',
-    mode   => '0755',
-
-    ensure  => present,
-  $mysql_password            = undef,
-    user    => $cron_discovery_all_user,
-    group  => 'root',
-      }
-      }
-        $my_packages = $packages
-    command => "${base_path}/discovery.php -h new >> /dev/null 2>&1",
-  $config_owner              = 'root',
-}
-    minute  => $cron_discovery_all_minute,
-
-      fail("Module observium is supported on osfamily Debian. Your osfamily is identified as ${::osfamily}")
-    default: {
-    name   => $base_path,
-    mode    => $rrd_mode,
-  $snmp_version              = 'v2c',
-      } ~>
-    group   => $rrd_group,
-  $cron_discovery_new_minute = '*/5',
-        ensure  => installed,
-
-  $rrd_group                 = 'root',
-    refreshonly => true, 
-    validate_hash($users)
-  $cron_discovery_all_user   = 'root',
-  cron { 'discovery-all':
-
-    mode    => $config_mode,
-  file { 'observium_path':
-  }
-    user    => $cron_poller_user,
-  if $users {
-  $rrd_mode                  = '0755',
-  }
-  $users                     = undef,
-    content => template('observium/config.php.erb'),
-
-      } else {
-
-    group   => $config_group,
-    # require => Package['observium_packages'],ss
-class observium (
-  $config_mode               = '0755',
-  $packages                  = 'USE_DEFAULTS',
-  $http_port                 = '80',
-    }
-  $rrd_path                  = '/opt/observium/rrd',
-    ensure => directory,
-
-      package { 'observium_packages':
-
-  $devices                   = undef,
-    'Debian': {
-    minute  => $cron_poller_minute,
-  $poller_threads            = '1',
-    path    => $rrd_path,
-    command => "${base_path}/poller-wrapper.py ${poller_threads} >> /dev/null 2>&1",
-        ensure => present,
-      }
-  $cron_poller_user          = 'root',
-  }
-    'RedHat': {
-  exec { 'update_db':
-      if $packages == 'USE_DEFAULTS' {
-    cwd         => $base_path,
-      exec { "extract_observium":
-  }
-  $communities               = ['public'],
-  }
-  $mysql_host                = undef,
-  }
-  $config_path               = '/opt/observium/config.php',
-        command => "/usr/bin/tar zxvf ${main_dir}/observium-community-latest.tar.gz -C $main_dir",
-    minute  => $cron_discovery_new_minute,
-  }
 # == Class: observium
+#
+# This module manages Observium
+#
+class observium (
+  $main_dir                  = '/opt',
+  $base_path                 = '/opt/observium',
+  $config_path               = '/opt/observium/config.php',
+  $config_mode               = '0755',
+  $config_owner              = 'root',
+  $config_group              = 'root',
+  $communities               = ['public'],
+  $devices                   = undef,
+  $http_port                 = '80',
+  $mysql_host                = undef,
+  $mysql_db                  = undef,
+  $mysql_user                = undef,
+  $mysql_password            = undef,
+  $packages                  = 'USE_DEFAULTS',
+  $poller_threads            = '1',
+  $rrd_path                  = '/opt/observium/rrd',
+  $rrd_mode                  = '0755',
+  $rrd_owner                 = 'root',
+  $rrd_group                 = 'root',
   $servername                = $::fqdn,
-    owner  => 'root',
-        $my_packages = $default_packages
-  case $::osfamily {
-  cron { 'poller':
+  $snmp_version              = 'v2c',
+  $users                     = undef,
+  $cron_discovery_all_hour   = '*/6',
+  $cron_discovery_all_minute = '33',
+  $cron_discovery_all_user   = 'root',
+  $cron_discovery_new_minute = '*/5',
+  $cron_discovery_new_user   = 'root',
+  $cron_poller_minute        = '*/5',
+  $cron_poller_user          = 'root',
 ) {
 
-    ensure  => directory,
-  }
-  if $devices {
-    observium::device { $devices: }
-  $cron_discovery_all_hour   = '*/6',
   include observium::apache
-  $main_dir                  = '/opt',
-    notify  => Exec['update_db'],
-  $config_group              = 'root',
-    require => File['observium_path'],
-    hour    => $cron_discovery_all_hour,
-    path        => '/bin:/usr/bin:/usr/local/bin',
 
-# This module manages Observium
-    create_resources('observium::user', $users)
-  file { 'observium_rrd_base':
-        source => "http://www.observium.org/observium-community-latest.tar.gz",
+  file { 'observium_path':
+    ensure => directory,
+    name   => $base_path,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
-  file { 'observium_config':
-#
+
+  case $::osfamily {
+    'Debian': {
+      $default_packages = 'observium'
+      if $packages == 'USE_DEFAULTS' {
+        $my_packages = $default_packages
+      } else {
+        $my_packages = $packages
+      }
+      package { 'observium_packages':
+        ensure  => installed,
+        name    => $my_packages,
+      }
+    }
+    'RedHat': {
       file { "$main_dir/observium-community-latest.tar.gz":
-
-  $cron_discovery_all_minute = '33',
-  $base_path                 = '/opt/observium',
+        ensure => present,
+        source => "http://www.observium.org/observium-community-latest.tar.gz",
+      } ~>
+      exec { "extract_observium":
+        command => "/usr/bin/tar zxvf ${main_dir}/observium-community-latest.tar.gz -C $main_dir",
+      }
+    }
+    default: {
+      fail("Module observium is supported on osfamily Debian. Your osfamily is identified as ${::osfamily}")
+    }
   }
+
+  if $users {
+    validate_hash($users)
+    create_resources('observium::user', $users)
+  }
+
+  if $devices {
+    validate_array($devices)
+    observium::device { $devices: }
+  }
+
+  file { 'observium_config':
+    ensure  => present,
+    path    => $config_path,
+    mode    => $config_mode,
+    owner   => $config_owner,
+    group   => $config_group,
+    content => template('observium/config.php.erb'),
+    # require => Package['observium_packages'],ss
+    notify  => Exec['update_db'],
+  }
+
+  file { 'observium_rrd_base':
+    ensure  => directory,
+    path    => $rrd_path,
+    mode    => $rrd_mode,
+    owner   => $rrd_owner,
+    group   => $rrd_group,
+    require => File['observium_path'],
+  }
+
+  exec { 'update_db':
+    path        => '/bin:/usr/bin:/usr/local/bin',
+    command     => 'php includes/update/update.php',
+    cwd         => $base_path,
+    refreshonly => true, 
+  }
+
+  cron { 'discovery-all':
+    command => "${base_path}/discovery.php -h all >> /dev/null 2>&1",
+    user    => $cron_discovery_all_user,
+    minute  => $cron_discovery_all_minute,
+    hour    => $cron_discovery_all_hour,
+  }
+
+  cron { 'discovery-new':
+    command => "${base_path}/discovery.php -h new >> /dev/null 2>&1",
+    user    => $cron_discovery_new_user,
+    minute  => $cron_discovery_new_minute,
+  }
+
+  cron { 'poller':
+    command => "${base_path}/poller-wrapper.py ${poller_threads} >> /dev/null 2>&1",
+    user    => $cron_poller_user,
+    minute  => $cron_poller_minute,
+  }
+}
